d4dd480e4e5b32c3c43d7f0a93c6eacfbfb4bc74
Alexio2137 <akazmierczyk_221i3@e.zseeim.edu.pl>
2024-03-12 08:58:05 +0000

    studentmachine send

--- cpp/cw2/main.cpp
+++ cpp/cw2/main.cpp
@@ -14,11 +14,16 @@ int main(){
 	float godziny = liczbaDni*24;
 	float minuty = godziny*60;
 	float sekundy = minuty*60;
-
+	
+	cout<<"Tygodnie: ";
 	cout<<tygodnie<<endl;
+	cout<<"Dni: ";
 	cout<<dni<<endl;
+	cout<<"Godziny: ";
 	cout<<godziny<<endl;
+	cout<<"Minuty: ";
 	cout<<minuty<<endl;
+	cout<<"Sekundy: ";
 	cout<<sekundy<<endl;
 	return 0;
 }
